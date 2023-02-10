81c9e91c6b4074a449acd32e9a5efa5be9bbceac
Vrishin Desai <vrishindesaiGM@gmail.com>
2023-02-11 04:21:41 +1100

    intial start of animation

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ import matplotlib.animation as animation
 
 
 fig, ax = plt.subplots()
-points, = ax.plot(np.random.rand(10), 'x')
-ax.set_ylim(0, 1)
+pitch_len = 120
+pitch_width = 75
+
+points, = ax.plot(np.random.uniform(high= pitch_len), np.random.uniform(high = pitch_width),'x')
+ax.set_ylim(0, pitch_width) 
+ax.set_xlim(0, pitch_len)
 
 def update(data):
     points.set_ydata(data)
+    points.set_xdata(data)
     return points,
 
 def generate_points():
     while True:
-        yield np.random.rand(10)  # change this
+        yield np.random.uniform(high=pitch_width, size=22)  # change this
 
 ani = animation.FuncAnimation(fig, update, generate_points, interval=300)
 ani.save('animation.gif', writer='imagemagick', fps=4);
